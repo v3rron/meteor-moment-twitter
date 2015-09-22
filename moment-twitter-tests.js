0e52aa814e3e5117d7471664f012203f05c98958
Ronen Verdi <go7hic@gmail.com>
2015-09-22 18:49:36 -0400

    initial commit

---
+++ moment-twitter-tests.js
@@ -0,0 +1,5 @@
+// Write your tests here!
+// Here is an example.
+Tinytest.add('example', function (test) {
+  test.equal(moment(new Date()).twitterShort(), '1s');
+});
---
+++ package.js
@@ -0,0 +1,24 @@
+Package.describe({
+  name: 'verron:moment-twitter',
+  version: '0.2.0',
+  // Brief, one-line summary of the package.
+  summary: '',
+  // URL to the Git repository containing the source code for this package.
+  git: '',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.0.1');
+  api.use('ecmascript');
+  api.addFiles('moment-twitter.js');
+});
+
+Package.onTest(function(api) {
+  api.use('ecmascript');
+  api.use('tinytest');
+  api.use('verron:moment-twitter');
+  api.addFiles('moment-twitter-tests.js');
+});
